c535a92b225a9b6abc4c7c6b713fa645a9c0fde7
Dustin Brothers <gentian.limited-0t@icloud.com>
2023-07-18 08:57:43 -0600

    Restructuring the pi setup script to contain all pertinent commands.

--- bin/pi_setup.sh
+++ bin/pi_setup.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-cd ~
-
+#################################################
 # Update and Install some basics that the pi needs...
 echo "Updating and upgrading..."
 sudo apt update ; sudo apt upgrade -Y
@@ -12,20 +11,56 @@ sudo apt install git
 echo "Install gvim..."
 sudo apt install vim -Y
 
-echo "Fetching and running the bin setup script..."
-wget https://raw.githubusercontent.com/rdustinb/WrenchAndGear/master/bin/bin_setup.sh
-chmod +x bin_setup.sh
-./bin_setup.sh
+echo "Install pip..."
+sudo apt install python3-pip -Y
 
+echo "Install pyserial via pip..."
+pip install pyserial
+
+#################################################
+echo "Setting up the terminal and the custom list all scripts..."
+mkdir -p ~/bin
+cd ~/bin
+
+# Fetch the ps1_coloring script
+echo "Fetching the PS1 coloring script..."
+wget https://raw.githubusercontent.com/rdustinb/WrenchAndGear/master/bin/ps1_coloring
+
+# Fetch the list all script
+echo "Fetching the list all script..."
+wget https://raw.githubusercontent.com/rdustinb/WrenchAndGear/master/bin/list_all.py
+
+# Adding aliases...
+echo "Adding aliases..."
+
+echo "alias ll='python3 ~/bin/list_all.py'" >> ~/.bashrc
+echo "source bin/ps1_coloring" >> ~/.bashrc
+
+echo "Please logout and back in for changes to take effect."
+
+#################################################
 echo "Fetching and running the vim setup script..."
 wget https://raw.githubusercontent.com/rdustinb/WrenchAndGear/master/env_and_vim/vim_setup.sh
 chmod +x vim_setup.sh
 ./vim_setup.sh
 
-echo "Fetching and running the pi services setup script..."
-wget https://raw.githubusercontent.com/rdustinb/WrenchAndGear/master/pi_services/pi_services_setup.sh
-chmod +x pi_services_setup.sh
-./pi_services_setup.sh
+#################################################
+# First grab the latest version of Docker, and install it
+echo "Fetching and installing Docker..."
+curl -sSL https://get.docker.com | sh
 
-echo "Rebooting..."
+# Adding the current user to the Docker group so compose files can be run by the user
+echo "Adding your username to the docker group..."
+sudo usermod -aG docker $$USER
+
+# Fetch the latest docker-compose file...
+echo "Fetching the latest docker-compose.yml file..."
+cd ~
+wget https://raw.githubusercontent.com/rdustinb/WrenchAndGear/master/pi_services/docker-compose.yml
+echo "After the reboot, edit the compose file to only include the containers needed to run on this system."
+echo "The launch Docker with:"
+echo "  docker compose up -d"
+echo ""
+
+echo "Rebooting the computer..."
 sudo reboot now
