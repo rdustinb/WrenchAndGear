c242c08da3525d4c5d139a943aa4e460407dc454
Dustin Brothers <gentian.limited-0t@icloud.com>
2024-01-11 17:40:18 -0800

    Vagrant cleanup script now uses the Virtual Box CLI to unregister inaccessible VMs that vagrant has destroyed. This prevents the VB GUI panel from having a bunch of missing VMs listed.

--- vagrant/vagrant_cleanup.sh
+++ vagrant/vagrant_cleanup.sh
@@ -4,3 +4,10 @@ vagrant destroy test -f
 echo -e "\nRemoving all traces of Vagrant VM configurations..."
 rm -fr ~/VirtualBox\ VMs/* ~/.vagrant.d .vagrant
 
+# Remove the VM information from VirtualBox
+echo -e "\nUnregistering all traces of Vagrant VMs from VirtualBox..."
+dead_vms=$(VBoxManage list vms | grep inaccessible | sed -e 's/{//g' | sed -e 's/}//g' | awk '{print $2}')
+
+for thisVM_UUID in ${dead_vms}; do
+  VBoxManage unregistervm $thisVM_UUID;
+done
