4002cd516b0da063b4a6905a6b01e0d44e93ecc5
Dustin Brothers <gentian.limited-0t@icloud.com>
2024-03-16 15:30:47 -0600

    Changing the name of the Packer template file for building a debian OS VM.

--- ansible/vm_test/tart/tart_setup_with_packer.sh
+++ ansible/vm_test/tart/tart_setup_with_packer.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # This uses the Packer tool to generate a vanilla VM with a Packer build file:
-packer build -var-file variables.pkrvars.hcl init.pkr.hcl
+packer build -var-file variables.pkrvars.hcl build_debianOS.pkr.hcl
