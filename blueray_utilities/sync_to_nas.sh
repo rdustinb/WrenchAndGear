7a35aa12d90d10bf8ff8918422df3107415f15cf
Dustin Brothers <gentian.limited-0t@icloud.com>
2024-03-25 17:57:29 -0600

    Updates to blueray scripts and git ignore.

--- blueray_utilities/sync_to_nas.sh
+++ blueray_utilities/sync_to_nas.sh
@@ -3,7 +3,10 @@
 fullpath="$1"
 
 # Get the variable configurations
-source config.sh
+source config.ini
+echo "Username: $username"
+echo "Primary Drive syncing to: $primaryDrive"
+echo "Secondary Drive syncing to: $backupDrive"
 
 # Working NAS
 scp "$fullpath" $username@$hostname:$primaryDrive/
