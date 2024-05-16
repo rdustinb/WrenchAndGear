6f23b7bb507299454996eff366568f75eb1e104c
Dustin Brothers <gentian.limited-0t@icloud.com>
2024-05-16 12:34:12 -0600

    Update to emulation files, checking in copies of software application floppy disk images. Launching Basilisk II on macOS now assumes the BasiliskII command includes the locations of the ROM and HDD images instead of the configuration file containing them.

--- emulation/Basilisk/run.sh
+++ emulation/Basilisk/run.sh
@@ -4,7 +4,7 @@
 THISFOLDER=`dirname $0`
 
 # Copy over the system configuration
-rsync -av ${THISFOLDER}/Configs/system7.5.3.config ~/.basilisk_ii_prefs
+rsync -av ${THISFOLDER}/Configs/system.config ~/.basilisk_ii_prefs
 
 # Run the System
 /Applications/BasiliskII.app/Contents/MacOS/BasiliskII &
