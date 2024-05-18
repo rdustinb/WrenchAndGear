aa8fa3b63bb9cf7e460ca66db6c3eff4bccd1319
Dustin Brothers <gentian.limited-0t@icloud.com>
2024-05-18 10:19:13 -0600

    Removing redundant sysem 7.5.5 update disks. Checking in updated run shell script.

--- emulation/Basilisk/run.sh
+++ emulation/Basilisk/run.sh
@@ -7,4 +7,6 @@ THISFOLDER=`dirname $0`
 rsync -av ${THISFOLDER}/Configs/system.config ~/.basilisk_ii_prefs
 
 # Run the System
-/Applications/BasiliskII.app/Contents/MacOS/BasiliskII &
+/Applications/BasiliskII.app/Contents/MacOS/BasiliskII \
+  --rom ~/Developer/WrenchAndGear/emulation/Basilisk/ROMs/ColorClassic-1MB.rom \
+  --disk ~/Developer/WrenchAndGear/emulation/Basilisk/HDDs/System7.dsk
