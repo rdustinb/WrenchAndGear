4ef05754063e20c5cc0f3465cd52aa6f3b16013a
Dustin Brothers <gentian.limited-0t@icloud.com>
2025-01-21 15:55:12 -0700

    Updated the UPS monitoring script to execute a shutdown command. Should be run by root or with sudo.

--- utilities/ups/upslevelshutdown.sh
+++ utilities/ups/upslevelshutdown.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
+DEBUG=0
 # This is the IP Address the SNMP Server daemon is running on:
 SERVERIP=192.168.85.100
+# SNMP Community
+SNMPCOMMUNITY=mynasups
 # This shouldn't ever change:
 OIDBASE=.1.3.6.1.4.1
 # This will be defined from the SNMP Server's MIB file:
@@ -9,18 +12,26 @@ UPSOFFSET=.44738.6
 UPSFIELDOFFSET=7
 
 # Shutdown Threshold in percentage
-NOTIFICATIONTHRESHOLD=90
-SHUTDOWNTHRESHOLD=50
+NOTIFICATIONTHRESHOLD=92
+SHUTDOWNTHRESHOLD=60
 
 # Call the SNMP Command
-UPSLEVEL=$(snmpwalk -v2c -c mynasups ${SERVERIP} ${OIDBASE}${UPSOFFSET} | head -n${UPSFIELDOFFSET} | tail -n1 | sed -e 's/ /~/g' | sed -e 's/:~/ /g' | awk '{print $2}')
+UPSLEVEL=$(snmpwalk -v2c -c ${SNMPCOMMUNITY} ${SERVERIP} ${OIDBASE}${UPSOFFSET} | head -n${UPSFIELDOFFSET} | tail -n1 | sed -e 's/ /~/g' | sed -e 's/:~/ /g' | awk '{print $2}')
+
+# Messages
+DISCHARGEMESSAGE="UPS Battery level has dropped to ${UPSLEVEL}%..."
+SHUTDOWNMESSAGE="The UPS Battery level of ${UPSLEVEL}% is lower than the defined shutdown threshold of ${SHUTDOWNTHRESHOLD}%."
+
+# Debug
+if [ "${DEBUG}" -eq "1" ]; then
+  echo "${DISCHARGEMESSAGE}"
+  echo "${SHUTDOWNMESSAGE}"
+fi
 
 # Determine if the UPS Battery Level is under the threshold
 if [ "${UPSLEVEL}" -lt "${SHUTDOWNTHRESHOLD}" ]; then
-  echo "The UPS Battery level of ${UPSLEVEL}% is lower than the defined shutdown threshold of ${SHUTDOWNTHRESHOLD}%."
-  echo "The system is shutting down..."
+  $(echo "${SHUTDOWNMESSAGE}" | tee /dev/kmsg)
+  $(shutdown now "${SHUTDOWNMESSAGE}")
 elif [ "${UPSLEVEL}" -lt "${NOTIFICATIONTHRESHOLD}" ]; then
-  echo "UPS Battery level has dropped to ${UPSLEVEL}%..."
-else
-  echo "UPS Battery level is full."
+  $(echo "${DISCHARGEMESSAGE}" | tee /dev/kmsg)
 fi
