bbe768be8e6b05a00ca1f209210ba529bbc42c4c
Dustin Brothers <gentian.limited-0t@icloud.com>
2025-02-27 10:44:59 -0700

    Changed up the monitor script to use DEBUG differently.

--- utilities/ups/upslevelshutdown.sh
+++ utilities/ups/upslevelshutdown.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-DEBUG=0
+DEBUG=1
 # This is the IP Address the SNMP Server daemon is running on:
 SERVERIP=192.168.85.100
 # SNMP Community
@@ -17,26 +17,30 @@ SHUTDOWNTHRESHOLD=60
 
 # Call the SNMP Command
 UPSLEVEL=$(snmpwalk -v2c -c ${SNMPCOMMUNITY} ${SERVERIP} ${OIDBASE}${UPSOFFSET} | head -n${UPSFIELDOFFSET} | tail -n1 | sed -e 's/ /~/g' | sed -e 's/:~/ /g' | awk '{print $2}')
+UPSRUNTIME=$(snmpwalk -v2c -c ${SNMPCOMMUNITY} ${SERVERIP} ${OIDBASE}${UPSOFFSET} | head -n${UPSFIELDOFFSET} | tail -n2 | head -n1 | sed -e 's/ /~/g' | sed -e 's/:~/ /g' | awk '{print $2}')
 
 # Messages
+RUNTIMEMESSAGE="UPS Battery runtime is ${UPSRUNTIME} minutes."
 NOMINALMESSAGE="UPS Battery level is ${UPSLEVEL}%."
 DISCHARGEMESSAGE="UPS Battery level has dropped to ${UPSLEVEL}%..."
 SHUTDOWNMESSAGE="The UPS Battery level of ${UPSLEVEL}% is lower than the defined shutdown threshold of ${SHUTDOWNTHRESHOLD}%."
 
 # Debug
 if [ "${DEBUG}" -eq "1" ]; then
-  echo "${DISCHARGEMESSAGE}"
-  echo "${SHUTDOWNMESSAGE}"
-fi
-
-# Determine if the UPS Battery Level is under the threshold
-if [ "${UPSLEVEL}" -lt "${SHUTDOWNTHRESHOLD}" ]; then
-  echo "${SHUTDOWNMESSAGE}" | tee /dev/kmsg
-  shutdown now "${SHUTDOWNMESSAGE}"
-elif [ "${UPSLEVEL}" -lt "${NOTIFICATIONTHRESHOLD}" ]; then
-  echo "${DISCHARGEMESSAGE}" | tee /dev/kmsg
+  echo "${RUNTIMEMESSAGE}"
+  if [ "${UPSLEVEL}" -lt "${SHUTDOWNTHRESHOLD}" ]; then
+    echo "${SHUTDOWNMESSAGE}"
+  elif [ "${UPSLEVEL}" -lt "${NOTIFICATIONTHRESHOLD}" ]; then
+    echo "${DISCHARGEMESSAGE}"
+  else
+    echo "${NOMINALMESSAGE}"
+  fi
 else
-  if [ "${DEBUG}" -eq "1" ]; then
-    echo "${NOMINALMESSAGE}" | tee /dev/kmsg
+  # Determine if the UPS Battery Level is under the threshold
+  if [ "${UPSLEVEL}" -lt "${SHUTDOWNTHRESHOLD}" ]; then
+    echo "${SHUTDOWNMESSAGE}" | tee /dev/kmsg
+    shutdown now "${SHUTDOWNMESSAGE}"
+  elif [ "${UPSLEVEL}" -lt "${NOTIFICATIONTHRESHOLD}" ]; then
+    echo "${DISCHARGEMESSAGE}" | tee /dev/kmsg
   fi
 fi
