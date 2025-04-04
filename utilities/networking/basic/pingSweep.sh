06468f15341e2a5950294d19ebe51d29b8531725
Dustin Brothers <gentian.limited-0t@icloud.com>
2025-04-04 11:30:42 -0600

    Updated ping sweep script with a verbose option and a branch to print out only a summary or everything.

--- utilities/networking/basic/pingSweep.sh
+++ utilities/networking/basic/pingSweep.sh
@@ -1,6 +1,8 @@
 HOSTNAME=$1
-PINGCOUNT=20
+PINGCOUNT=$2
+VERBOSE=$3
 PINGDELAY_SECONDS=0.005
+LOGFILE=testOut.log
 
 # -f floods the pings out as soon as the previous response returns, up to 100 times per second or a period of 0.01
 # seconds.
@@ -8,8 +10,15 @@ PINGDELAY_SECONDS=0.005
 # -i x.xx is the amount of delay to wait from when the previous response returns to when the next request is sent.
 # Minimum of 0.002 seconds unless sudo is used.
 
+usage () {
+  echo -e "Usage:"
+  echo -e "\tpingTest.sh <hostname> <ping packet count> <verbose Y or N>"
+  exit
+}
+
 declare -a PAYLOAD_BYTES_ARRAY=(
   56
+  120
   248
   504
   1016
@@ -17,21 +26,29 @@ declare -a PAYLOAD_BYTES_ARRAY=(
   2040
 )
 
-if [ "${HOSTNAME}" == "" ]; then
-  echo -e "Please provide a hostname to test..."
-  echo -e "Usage:"
-  echo -e "\tpingTest.sh <hostname>"
-  exit
+# Check for incorrect call of the script...
+if [ "${HOSTNAME}" == "" ] || [ "${PINGCOUNT}" == "" ] || [ "${VERBOSE}" == "" ]; then
+  usage
 fi
 
+# Run the ping sweep...
+(
 echo -e "\n###################################################"
-echo -e "Hostname is: ${HOSTNAME}"
+echo -e "# Target: ${HOSTNAME}"
 echo -e "###################################################"
 
 for PAYLOAD_BYTES in "${PAYLOAD_BYTES_ARRAY[@]}"; do
   echo -e "\n"
   echo -e "###################################################"
-  echo -e "Byte payload of ${PAYLOAD_BYTES} bytes"
+  echo -e "# ${PAYLOAD_BYTES} byte payload"
   echo -e "###################################################"
   ping -c ${PINGCOUNT} -i ${PINGDELAY_SECONDS} -s ${PAYLOAD_BYTES} ${HOSTNAME}
 done
+) > ${LOGFILE}
+
+# Dump the results...
+if [ "${VERBOSE}" == "Y" ]; then
+  cat ${LOGFILE}
+else
+  grep -e "packet loss" -e "#" ${LOGFILE}
+fi
